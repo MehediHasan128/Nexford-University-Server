bd87be7b60d934c3d1d6cf96ed01f88509374010
MehediHasan128 <mehedihasan12926@gmail.com>
2024-11-07 22:58:46 +0600

    add validation request function which check the data

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -1,53 +1,60 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 const userNameValidationSchema = z.object({
-    firstName: z.string().max(10, { message: 'First name must be less then 10 characters' }),
-    middleName: z.string().max(10, { message: 'Middle name must be less then 10 characters' }).optional(),
-    lastName: z.string().max(10, { message: 'Last name must be less then 10 characters' })
+  firstName: z
+    .string()
+    .max(10, { message: 'First name must be less then 10 characters' }),
+  middleName: z
+    .string()
+    .max(10, { message: 'Middle name must be less then 10 characters' })
+    .optional(),
+  lastName: z
+    .string()
+    .max(10, { message: 'Last name must be less then 10 characters' }),
 });
-
 
 const guardianValidationSchema = z.object({
-    fatherName: z.string().optional(),
-    fatherOccupation: z.string().optional(),
-    fatherContactNo: z.string().optional(),
+  fatherName: z.string().optional(),
+  fatherOccupation: z.string().optional(),
+  fatherContactNo: z.string().optional(),
 
-    motherName: z.string().optional(),
-    motherOccupation: z.string().optional(),
-    motherContactNo: z.string().optional()
+  motherName: z.string().optional(),
+  motherOccupation: z.string().optional(),
+  motherContactNo: z.string().optional(),
 });
-
 
 const localGuardianValidationSchema = z.object({
-    name: z.string().optional(),
-    occupation: z.string().optional(),
-    contactNo: z.string().optional()
+  name: z.string().optional(),
+  occupation: z.string().optional(),
+  contactNo: z.string().optional(),
 });
-
-
-
 
 const createStudentValidationSchema = z.object({
-    id: z.string(),
-    name: userNameValidationSchema,
-    gender: z.enum(['male', 'female']),
-    email: z.string().email({ message: 'Invalid email address' }),
-    dateOfBirth: z.string().datetime({ message: 'Date must be UTC' }),
-    contactNo: z.string(),
-    emergencyContactNo: z.string(),
-    bloodGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']).optional(),
-    presentAddress: z.string(),
-    permanentAddress: z.string(),
-    guardian: guardianValidationSchema,
-    localGuardian: localGuardianValidationSchema,
-    isActive: z.enum(['active', 'blocked']).default('active'),
-    isDeleted: z.boolean().default(false)
+  body: z.object({
+    password: z
+      .string({ invalid_type_error: 'Password must be string' })
+      .min(8, 'Password must be at least 8 characters long')
+      .max(20, 'Password cant not more than 20 characters')
+      .optional(),
+    student: z.object({
+      name: userNameValidationSchema,
+      gender: z.enum(['male', 'female']),
+      email: z.string().email({ message: 'Invalid email address' }),
+      dateOfBirth: z.string().datetime({ message: 'Date must be UTC' }),
+      contactNo: z.string(),
+      emergencyContactNo: z.string(),
+      bloodGroup: z
+        .enum(['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'])
+        .optional(),
+      presentAddress: z.string(),
+      permanentAddress: z.string(),
+      guardian: guardianValidationSchema,
+      localGuardian: localGuardianValidationSchema,
+      isDeleted: z.boolean().default(false),
+    }),
+  }),
 });
 
-
-
-// export const StudentValidation = {
-//     createStudentValidationSchema
-// }
-
-export default createStudentValidationSchema
+export const StudentValidation = {
+  createStudentValidationSchema,
+};
--- src/app/modules/user/user.router.ts
+++ src/app/modules/user/user.router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { UserController } from './user.controller';
+import validateRequest from '../../middlwares/validationRequest';
+import { StudentValidation } from '../student/student.validation';
 
 const router = express.Router();
 
-router.post('/create-student', UserController.createStudentUser);
+router.post('/create-student', validateRequest(StudentValidation.createStudentValidationSchema), UserController.createStudentUser);
 
 export const UserRoutes = router;
